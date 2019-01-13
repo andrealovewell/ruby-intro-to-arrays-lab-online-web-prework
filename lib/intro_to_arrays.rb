b4e2ee57986fa44f619815f31f16f27fbf4a2fb0
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-13 00:19:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,9 @@ end
 def array_with_two_elements
   my_two_array = Array.new(2)
 end
+
+def first_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift[1]
+end
+  
