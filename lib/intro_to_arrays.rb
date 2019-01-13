d81defa64ac6a4281e86f16e84dded354e56563e
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-13 00:35:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,3 +11,11 @@ taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods
 def first_element(taylor_swift)
   taylor_swift[0]
 end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift[-1]
+end
