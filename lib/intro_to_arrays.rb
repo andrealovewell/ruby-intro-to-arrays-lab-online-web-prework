262765c7bdac1e58e75f4b167ab5422292c2b39b
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-13 00:39:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -25,3 +25,13 @@ south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 def first_element_with_array_methods(south_east_asia)
   south_east_asia.first
 end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end
+
+programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+
+def length_of_array(programming_languages)
+  programming_languages.length
+end
