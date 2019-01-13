3ec90990efdcb2b0ef575973639de87e9376da55
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-13 00:12:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,6 +2,6 @@ def instantiate_new_array
   my_new_array =Array.new
 end
 
-def array_with_two_elements(object1, object2)
-  my_new_array = [object1, object2]
+def array_with_two_elements
+  my_new_array [2]
 end
